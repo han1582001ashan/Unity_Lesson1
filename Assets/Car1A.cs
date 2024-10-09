846749ba7075c3c1b8e2b9c145e43a22a2b27e0f
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-10-09 20:23:46 +0700

    Update Car1A.cs

--- Assets/Car1A.cs
+++ Assets/Car1A.cs
@@ -10,42 +10,50 @@ using UnityEngine.UIElements;
 public class Car1A : MonoBehaviour
 {
   // Start is called before the first frame update
-public Vector3 AnglesRotate= new Vector3(0,90,0);
-public float rotationSpeed;
+  public Vector3 AnglesRotate = new Vector3(0, 90, 0);
+  public float rotationSpeed = 50f;
   [SerializeField] List<GameObject> wayLimitPoints;
-  float speed =2;
-int index =0;
+  float speed = 2;
+  int index = 0;
   void Start()
   {
-   
+
   }
 
   // Update is called once per frame
   void Update()
   {
-    
     Vector3 destination = wayLimitPoints[index].transform.position;
-   Vector3 newpos=  Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
-
-    transform.position= newpos;
-
     float distance = Vector3.Distance(transform.position, destination);
-    if(distance<=0.05){
-    if(index< wayLimitPoints.Count-1){
-      index++;
-    transform.Rotate(AnglesRotate);
 
-    }
-      else {
-      index=0;
-     transform.Rotate(AnglesRotate);
-    } 
-    }
-    
+    Vector3 newpos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
    
+    transform.position = newpos;
+    Vector3 relativePos= wayLimitPoints[index].transform.position - transform.position; 
+
+
+    Quaternion targetRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+    transform.rotation= Quaternion.Lerp(transform.rotation, targetRotation, 10* Time.deltaTime);
     
-    
-    
+
+    if (distance <= 0.2)
+    {
+      if (index < wayLimitPoints.Count - 1)
+      {
+        index++;
+// transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+      }
+      else
+      {
+        index = 0;
+
+      }
+    }
+
+
+
+
+
 
   }
 
